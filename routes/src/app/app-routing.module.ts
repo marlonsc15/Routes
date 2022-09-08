5efb703af9a1698a5924a8a29cac4bebfc34780e
marlonsc15 <marlonsantoscorreia@gmail.com>
2022-09-08 13:27:25 -0300

    criação do projeto  e componentes sobre/home/error

---
+++ routes/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { HomeComponent } from './pages/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PageErrorComponent } from './pages/page-error/page-error.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent,pathMatch: 'full' },
+  { path: 'sobre', component: SobreComponent },
+  { path: '404', component: PageErrorComponent },
+  { path: '**', redirectTo: '404' }
+];
+ 
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
